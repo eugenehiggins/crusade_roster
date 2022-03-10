9a1cc063d3d7b76387f34ef1de03e6d018760bcc
Gene Higgins <genotype@gknow.com>
2022-03-10 14:32:10 -0700

    added more complex serializer https://www.django-rest-framework.org/tutorial/1-serialization/

--- order_of_battle/urls.py
+++ order_of_battle/urls.py
@@ -3,13 +3,14 @@ from rest_framework import routers
 
 from . import views
 
-# app_name = 'armies'
+app_name = 'armies'
 
-router = routers.DefaultRouter()
-router.register(r'armies', views.ArmiesViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'armies', views.ArmiesViewSet)
 
 urlpatterns = [
     # path('', views.index, name='index'),
     # path('<int:id>/', views.detail, name='detail'),
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', views.army_list),
+    path('<int:pk>/', views.army_detail),
 ]
--- order_of_battle/views.py
+++ order_of_battle/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from rest_framework import viewsets
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
 from order_of_battle.models import Unit, Army
 from order_of_battle.serializers import ArmySerializer
@@ -21,6 +22,52 @@ def detail(request, id):
     return render(request, 'unit/detail.html', {'order_of_battle': unit})
 
 
-class ArmiesViewSet(viewsets.ModelViewSet):
-    queryset = Army.objects.all()
-    serializer_class = ArmySerializer
+@csrf_exempt
+def army_list(request):
+    """
+    List all armies, or create a new one
+    """
+
+    if request.method == 'GET':
+        armies = Army.objects.all()
+        serializer = ArmySerializer(armies, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArmySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def army_detail(request, pk):
+    """
+    Retrieve, update or delete an army
+    :param request:
+    :param pk:
+    :return:
+    """
+
+    try:
+        army = Army.objects.get(pk=pk)
+    except Army.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArmySerializer(army)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArmySerializer(army, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        army.delete()
+        return HttpResponse(status=204)
